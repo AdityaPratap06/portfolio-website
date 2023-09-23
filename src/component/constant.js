f031f2dcee58c5dcce54ddfe103930127ca19644
AdityaPratap06 <pratapsinghaditya94@gmail.com>
2023-09-23 16:47:36 +0530

    skills

---
+++ src/component/constant.js
@@ -0,0 +1,8 @@
+export const FrontEndIcon = [
+    { title: "HTML", icon: "/html5-icon.png" },
+    { title: "CSS", icon: "/css-icon.png" },
+    { title: "JavaScript", icon: "/js-icon.png" },
+    { title: "ReactJS", icon: "/react-icon.png" },
+    { title: "NextJS", icon: "/nextjs-icon.png" },
+    { title: "Redux", icon: "/redux-icon.png" },
+]
--- src/features/Home/Skills.js
+++ src/features/Home/Skills.js
@@ -1,10 +1,29 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 export const Skills = () => {
     return (
-        <Flex h="80vh">
-
-        </Flex>
+        <Box h="80vh">
+            <Flex w="100%">
+                <Box w="15%">
+                </Box>
+                <Box w="70%">
+                    <Flex>
+                        <Flex>
+                            <Text>Front-end:</Text>
+                        </Flex>
+                        <Flex>
+                            {map(FrontEndIcon, icon => (
+                                <Box>
+                                    {/* <Image src={icon.icon} alt={icon.title} /> */}
+                                </Box>
+                            ))}
+                        </Flex>
+                    </Flex>
+                </Box>
+                <Box w="15%">
+                </Box>
+            </Flex>
+        </Box>
     )
 }
