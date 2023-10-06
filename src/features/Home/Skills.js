5160a610a2f160b0b5595be89aa7840372aaa3f2
AdityaPratap06 <pratapsinghaditya94@gmail.com>
2023-10-06 23:26:33 +0530

    skills responsive

--- src/features/Home/Skills.js
+++ src/features/Home/Skills.js
@@ -1,3 +1,4 @@
+import { CenterX } from "@/common/CenterX";
 import { BackEndIcon, FrontEndIcon, OtherIcon } from "@/component/constant";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import { map } from "lodash";
@@ -5,47 +6,45 @@ import React from "react";
 
 export const Skills = () => {
     return (
-        <Box p={20} id={"skills"}>
+        <Box py={20} id={"skills"}>
             <Flex justify={"center"} fontSize={30} fontWeight={"bold"}>
                 <Text px={2} borderBottom={"3px solid"}>Skills</Text>
             </Flex>
-            <Flex w="100%">
-                <Box w="15%">
-                </Box>
-                <Box w="70%" mt={10}>
-                    <Flex>
-                        <Flex w="50%">
-                            <Text fontSize={36} fontWeight={"semibold"}>Front-end:</Text>
+            <CenterX>
+                <Box w="100%" mt={10}>
+                    <Flex flexDir={{ base: "column", md: "row" }}>
+                        <Flex w={{ base: "100%", md: "50%" }}>
+                            <Text fontSize={{ base: 24, md: 24, lg: 30, xl: 36 }} fontWeight={"semibold"}>Front-end:</Text>
                         </Flex>
-                        <Flex w="50%" flexWrap={"wrap"}>
+                        <Flex w={{ base: "100%", md: "50%" }} flexWrap={"wrap"}>
                             {map(FrontEndIcon, icon => (
-                                <Box w="20%" p={2} align="center">
+                                <Box w={95} p={2} align="center">
                                     <Image w="100%" h="60pt" objectFit={"contain"} src={icon.icon} alt={icon.title} />
                                     <Text mt={2} fontSize={17} fontWeight={"semibold"}>{icon.title}</Text>
                                 </Box>
                             ))}
                         </Flex>
                     </Flex>
-                    <Flex mt={5}>
-                        <Flex w="50%">
-                            <Text fontSize={36} fontWeight={"semibold"}>Back-end:</Text>
+                    <Flex mt={5} flexDir={{ base: "column", md: "row" }}>
+                        <Flex w={{ base: "100%", md: "50%" }}>
+                            <Text fontSize={{ base: 24, md: 24, lg: 30, xl: 36 }} fontWeight={"semibold"}>Back-end:</Text>
                         </Flex>
-                        <Flex w="50%" flexWrap={"wrap"}>
+                        <Flex w={{ base: "100%", md: "50%" }} flexWrap={"wrap"}>
                             {map(BackEndIcon, icon => (
-                                <Box w="20%" p={2} align="center">
+                                <Box w={95} p={2} align="center">
                                     <Image w="100%" h="60pt" objectFit={"contain"} src={icon.icon} alt={icon.title} />
                                     <Text mt={2} fontSize={17} fontWeight={"semibold"}>{icon.title}</Text>
                                 </Box>
                             ))}
                         </Flex>
                     </Flex>
-                    <Flex mt={5}>
-                        <Flex w="50%">
-                            <Text fontSize={36} fontWeight={"semibold"}>Other:</Text>
+                    <Flex mt={5} flexDir={{ base: "column", md: "row" }}>
+                        <Flex w={{ base: "100%", md: "50%" }}>
+                            <Text fontSize={{ base: 24, md: 24, lg: 30, xl: 36 }} fontWeight={"semibold"}>Other:</Text>
                         </Flex>
-                        <Flex w="50%" flexWrap={"wrap"}>
+                        <Flex w={{ base: "100%", md: "50%" }} flexWrap={"wrap"}>
                             {map(OtherIcon, icon => (
-                                <Box w="20%" p={2} align="center">
+                                <Box w={95} p={2} align="center">
                                     <Image w="100%" h="60pt" objectFit={"contain"} src={icon.icon} alt={icon.title} />
                                     <Text mt={2} fontSize={17} fontWeight={"semibold"}>{icon.title}</Text>
                                 </Box>
@@ -53,9 +52,7 @@ export const Skills = () => {
                         </Flex>
                     </Flex>
                 </Box>
-                <Box w="15%">
-                </Box>
-            </Flex>
+            </CenterX>
         </Box>
     )
 }
