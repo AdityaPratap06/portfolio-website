c82af51f15c9c94015c3acbafe765ee1b644a35b
AdityaPratap06 <pratapsinghaditya94@gmail.com>
2023-10-06 23:58:39 +0530

    footer bottom responsive

--- src/features/Home/FooterBottom.js
+++ src/features/Home/FooterBottom.js
@@ -7,11 +7,12 @@ export const FooterBottom = () => {
         window.open("https://www.linkedin.com/in/aditya-mern-full-stack-developer/")
     }
     return (
-        <Flex justify={"center"} bg="#000727" color={"white"} py={5} borderTop={"1px solid"} borderColor={"whiteAlpha.300"}>
-            Copyright
-            <Flex mx={2} align={"center"}><AiOutlineCopyright /></Flex>
-            2023 - All Right Reserved
-
+        <Flex justify={"center"} align={"center"} bg="#000727" color={"white"} py={5} flexDir={{ base: "column", sm: "row" }} fontSize={{ base: 10, sm: 12, md: 16 }} borderTop={"1px solid"} borderColor={"whiteAlpha.300"}>
+            <Flex>
+                Copyright
+                <Flex mx={2} align={"center"}><AiOutlineCopyright /></Flex>
+                2023 - All Right Reserved
+            </Flex>
             <Flex ml={3}>
                 Made by <Text ml={2} borderBottom={"2px solid white"} onClick={toLinkedIn} cursor={"pointer"} _hover={{ color: "blue.200", borderColor: "blue.200" }}>Aditya Pratap Singh</Text>
             </Flex>
