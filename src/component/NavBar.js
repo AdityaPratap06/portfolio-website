f4f702d2f9d75c8c2cf2ec886e229e022375d578
AdityaPratap06 <pratapsinghaditya94@gmail.com>
2023-10-06 12:19:10 +0530

    navbar responsive

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -8,8 +8,8 @@ import React from "react";
 export const NavBar = () => {
     return (
         <Box bg={"black"}>
-            <Flex justify={"space-between"} py={2} px={5}>
-                <HStack spacing={10} color="white">
+            <Flex flexDir={{ base: "column", md: "row" }} justify={"space-between"} py={2} px={5}>
+                <HStack flexDir={{ base: "column", sm: "row" }} align={"flex-end"} spacing={{ base: 1, sm: 10 }} fontSize={{ base: 12, sm: 16 }} color="white">
                     <Flex align="center">
                         <AiOutlineMail color="blue.500" />
                         <Text ml={2}>pratapsinghaditya94@gmail.com</Text>
@@ -19,7 +19,7 @@ export const NavBar = () => {
                         <Text ml={2}>+91 8077344281</Text>
                     </Flex>
                 </HStack>
-                <HStack>
+                <HStack justify={"flex-end"} mt={{ base: 1, sm: 0 }}>
                     <IconX bg={"blue.600"} icon={<AiFillLinkedin />} link={"https://www.linkedin.com/in/aditya-mern-full-stack-developer/"} />
                     <IconX bg={"blue.600"} icon={<FaFacebookF />} link={"https://www.facebook.com/aditya.jadon.52056"} />
                     <IconX bg={"blue.600"} icon={<AiFillGithub />} link={"https://github.com/AdityaPratap06"} />
@@ -32,7 +32,7 @@ export const NavBar = () => {
 
 const IconX = ({ icon, link }) => {
     return (
-        <Flex w={30} h={30} border={"3px solid"} bg="white" borderRadius={100} borderColor={"gray.200"} align="center" position="relative" justify="center" role="group" overflow={"hidden"} onClick={() => window.open(link)}>
+        <Flex w={{ base: 22, sm: 30 }} h={{ base: 22, sm: 30 }} border={"3px solid"} bg="white" borderRadius={100} borderColor={"gray.200"} align="center" position="relative" justify="center" role="group" overflow={"hidden"} onClick={() => window.open(link)}>
             <Flex position="absolute" w={"100%"} zIndex={1} h={"0"} bottom={0} _groupHover={{ h: "100%", bg: "#008FFF", transition: "height .3s linear" }} />
             <Flex position="relative" color="#262626" zIndex={9} transition='all 0.5s' _groupHover={{ color: "#fff", transform: "rotateY(360deg)" }}>
                 {icon}
